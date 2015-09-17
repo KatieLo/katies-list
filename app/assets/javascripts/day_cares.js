a52ce65e5345d8f2f942dfcb515722da07102aef
Katie Womersley <katielouw@gmail.com>
2015-09-17 15:52:19 -0700

    add basic streetview

---
+++ app/assets/javascripts/day_cares.js
@@ -0,0 +1,15 @@
+//Place all the behaviors and hooks related to the matching controller here.
+//All this logic will automatically be available in application.js.
+
+// On document ready, get a starter Google map to show while we wait for filters 
+$(function() {
+	getInitialMap();
+});
+
+function getInitialMap() {
+	var address = $(".street-address").html();
+	console.log("address", address);
+	var src = "https://maps.googleapis.com/maps/api/streetview?location=" + address +"&size=600x400";
+	console.log("src", src);
+    $('#map').css({"background-image":"url('"+ src +"')", 'background-repeat': 'no-repeat', 'background-size' : 'cover'});
+};
