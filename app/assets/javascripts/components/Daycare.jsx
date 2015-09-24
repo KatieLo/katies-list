37b4097c77842ddf6167e3c7d8fe89bf991045ea
Katie Womersley <katielouw@gmail.com>
2015-09-24 11:27:23 -0700

    make parent component with child components that render the basic JSX and remove the erb items

---
+++ app/assets/javascripts/components/Daycare.jsx
@@ -0,0 +1,20 @@
+var Daycare = React.createClass({
+	render: function() {
+		return (
+			<div className="row">
+				<div className="col-xs-12">
+					<div className="row">
+						<div className="col-xs-12 col-sm-6">
+							<div className="streetview-img"></div>
+							<h3 className="daycare-name">The Name</h3>
+						</div>
+						<div className="col-xs-12 col-sm-6">
+							<h6 className="street-address">The street address,the city</h6>
+							<p>the description</p>
+						</div>
+					</div>	
+				</div>
+			</div>
+			)
+	}
+});
---
+++ app/assets/javascripts/components/DaycareListings.js.jsx
@@ -0,0 +1,9 @@
+var DayCareListings = React.createClass({
+	render: function() {
+		return (
+			<div>
+		        < Daycare />
+		      </div>
+			)
+	}
+});
