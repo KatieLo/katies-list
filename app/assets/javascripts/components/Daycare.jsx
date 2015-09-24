77a72b2b4b76b6b890c55be1f8392147cdeafeea
Katie Womersley <katielouw@gmail.com>
2015-09-24 15:04:38 -0700

    render each component with data

--- app/assets/javascripts/components/Daycare.jsx
+++ app/assets/javascripts/components/Daycare.jsx
@@ -6,11 +6,11 @@ var Daycare = React.createClass({
 					<div className="row">
 						<div className="col-xs-12 col-sm-6">
 							<div className="streetview-img"></div>
-							<h3 className="daycare-name">The Name</h3>
+							<h3 className="daycare-name">{this.props.name}</h3>
 						</div>
 						<div className="col-xs-12 col-sm-6">
-							<h6 className="street-address">The street address,the city</h6>
-							<p>the description</p>
+							<h6 className="street-address">{this.props.street},{this.props.city}</h6>
+							<p>{this.props.description}</p>
 						</div>
 					</div>	
 				</div>
--- app/assets/javascripts/components/DaycareListings.js.jsx
+++ app/assets/javascripts/components/DaycareListings.js.jsx
@@ -1,9 +1,16 @@
 var DayCareListings = React.createClass({
-	render: function() {
-		return (
-			<div>
-		        < Daycare />
-		      </div>
-			)
-	}
+	
+	render: function () {
+    var dayCareNodes = this.props.data.map(function (daycare, index) {
+      return (
+        <Daycare name={daycare.name} description={daycare.description} street={daycare.street}  city={daycare.city} key={index} />
+        );
+    });
+
+    return (
+      <div className="DayCareList">
+        {dayCareNodes}
+      </div>
+      );
+  }
 });
