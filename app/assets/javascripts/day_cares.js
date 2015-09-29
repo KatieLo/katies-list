8227525b24c9390f01239e367c9cdd9ecc2a6312
Katie Womersley <katielouw@gmail.com>
2015-09-28 18:18:36 -0700

    notes from Niel

--- app/assets/javascripts/day_cares.js
+++ app/assets/javascripts/day_cares.js
@@ -1,6 +1,12 @@
 //Place all the behaviors and hooks related to the matching controller here.
 //All this logic will automatically be available in application.js.
 
+// On document ready
+$(function() {
+	// make map and listings the right height
+	
+});
+
 // Whenever the window height changes, we must resize the map and listings
 $(window).resize(function() {
 	resizeContainerHeights();
@@ -20,3 +26,14 @@ function resizeContainerHeights() {
 	$('#map').css('height', mapHeight + 'px');
 	$('#left-side').css('height', mapHeight + 'px');
 }
+
+/*
+controller {
+	initialize 	-> renders all components in loading state
+				-> ajax call to get data
+				-> store in local vars
+				-> call this.render
+	filter_by_filters -> filters data then this.render
+	filter_by_map_coords -> filters data then this.render
+	render -> React.render
+}*/
