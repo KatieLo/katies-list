861628c07686a657de1c2c8a444a6fb93e9dc436
Katie Womersley <katielouw@gmail.com>
2015-09-29 17:44:19 -0700

    add a basic google map to the map component

--- app/assets/javascripts/components/DayCareMap.js.jsx
+++ app/assets/javascripts/components/DayCareMap.js.jsx
@@ -1,6 +1,14 @@
 var DayCareMap = React.createClass({
 	componentDidMount: function() {
 		// Use Google JS Stuff to init a map in the div
+		var myLatlng = new google.maps.LatLng(37.4292, -122.1381);
+		var mapOptions = {
+		  zoom: 12,
+		  center: myLatlng,
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("map"),
+		    mapOptions);
 	},
 
 	render: function() {
