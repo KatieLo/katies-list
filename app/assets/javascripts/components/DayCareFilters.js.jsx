e6592e29b698c8fe3e5bed34315089939d554603
Katie Womersley <katielouw@gmail.com>
2015-10-27 16:54:12 -0700

    imporve styling and responsiveness of filters component

--- app/assets/javascripts/components/DayCareFilters.js.jsx
+++ app/assets/javascripts/components/DayCareFilters.js.jsx
@@ -5,25 +5,31 @@ var DayCareFilters = React.createClass({
 		return (
 			<div id="filters">
 				<div className="row">
-					<div className="hidden-xs col-md-8 filter-label">
-						<label>Age Range (Months)</label>
-						<input id="age-range" type="range" min="1" max="60" step="1" />
-						<span className="label-min">1 month</span>
-						<span className="label-max">60 months (5 years)</span>
+					<div className="hidden-xs col-md-12 filter-label">
+						<div className="filter">
+							<label>Age Range (Months)</label>
+							<input id="age-range" type="range" min="1" max="60" step="1" />
+							<span className="label-min">1 month</span>
+							<span className="label-max">60 months (5 years)</span>
+						</div>
 					</div>
 				</div>
 				<div className="row">
-					<div className="hidden-xs col-md-8 filter-label">
-						<label>Price Range ($100s)</label>
-						<input id="age-range" type="range" min="500" max="5000" step="100" />
-						<span className="label-min">$10</span>
-						<span className="label-max">$2000</span>
+					<div className="hidden-xs col-md-12 filter-label">
+						<div className="filter">
+							<label>Price Range ($100s)</label>
+							<input id="age-range" type="range" min="500" max="5000" step="100" />
+							<span className="label-min">$10</span>
+							<span className="label-max">$2000</span>
+						</div>
 					</div>
 				</div>
 				<div className="row">
-					<div className="hidden-xs col-md-8 filter-label last">
-						<label>Only show daycares with no waitlist</label>
-						<input id="age-range" type="checkbox"/>
+					<div className="hidden-xs col-md-12 filter-label last">
+						<div className="filter">
+							<input id="age-range" type="checkbox"/>
+							<label>Only show daycares with no waitlist</label>
+						</div>
 					</div>
 				</div>
 			</div>
